8214728a7a131fa0e0a0e3d26d636f32f1093361
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2023-11-23 18:46:27 +0530

    first commit

---
+++ routes/blogRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { getAllBlogsController, createBlogController, updateBlogController, getBlogByIdController, deleteBlogController, userBlogController } = require('../controllers/blogControllers')
+
+//router object
+const router = express.Router()
+
+//routes
+router.get('/all-blog', getAllBlogsController)
+
+//POST || create blog
+router.post('/create-blog', createBlogController)
+
+//PUT || update blog
+router.put('/update-blog/:id', updateBlogController)
+
+//GET || Single Blog Details
+router.get('/get-blog/:id', getBlogByIdController)
+
+//DELETE || delete Blog
+router.delete('/delete-blog/:id', deleteBlogController)
+
+
+//GET || user blog
+router.get('/user-blog/:id', userBlogController)
+module.exports = router
